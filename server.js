be72001c3ea76981357e3b568cbb26f9c7c333b6
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-15 19:22:02 -0700

    created books model file

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/booksearch",
+    process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
