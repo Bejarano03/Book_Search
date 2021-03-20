85f5d8ecab5658bb380cccab1f7ae8540a9b14ef
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-20 00:12:33 -0700

    finished setting up routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose")
+const routes = require("./routes");
 
 app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
@@ -11,16 +12,13 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
+app.use(routes);
+
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
     { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
-// Define API routes
-
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 app.listen(PORT, () => {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
