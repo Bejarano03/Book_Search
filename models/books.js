75e809e8a59ac94bc4f47e86b5938d3589477756
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-20 00:00:25 -0700

    created book schema and exported to index.js so it connects

--- models/books.js
+++ models/books.js
@@ -2,5 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const books = new mongoose.Schema({
-    
-})
+    title: { type: String, required: true},
+    author: { type: String, required: true},
+    sypnosis: String,
+    date: {type: Date, default: Date.now}
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
