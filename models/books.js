f58c54803ac07d4f9ac44ced60c9edc9b01b9b14
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-20 00:01:46 -0700

    added other fields necessaryf or our form

--- models/books.js
+++ models/books.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const books = new mongoose.Schema({
+    authors: {type: String, required: true},
     title: { type: String, required: true},
-    author: { type: String, required: true},
-    sypnosis: String,
+    authors: { type: String, required: true},
+    description: String,
+    link: { type: String},
+    image: {type: String},
     date: {type: Date, default: Date.now}
 });
 
